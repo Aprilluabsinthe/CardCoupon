7a6b91bf2106f5968f05ee1586d6fc5b07b095a5
Di Lu <dilu@andrew.cmu.edu>
2021-06-28 21:00:06 -0400

    add Kafka send logic

--- src/main/java/com/cardcoupon/passbook/service/impl/MerchantsServImpl.java
+++ src/main/java/com/cardcoupon/passbook/service/impl/MerchantsServImpl.java
@@ -1,16 +1,22 @@
 package com.cardcoupon.passbook.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.cardcoupon.passbook.constant.Constants;
 import com.cardcoupon.passbook.constant.ErrorCode;
 import com.cardcoupon.passbook.dao.MerchantsDao;
+import com.cardcoupon.passbook.entity.Merchants;
 import com.cardcoupon.passbook.service.IMerchantsServ;
 import com.cardcoupon.passbook.vo.CreateMerchantsRequest;
 import com.cardcoupon.passbook.vo.CreateMerchantsResponse;
 import com.cardcoupon.passbook.vo.PassTemplate;
 import com.cardcoupon.passbook.vo.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * Implementation for Interfaces of Merchants Service
@@ -21,12 +27,21 @@ public class MerchantsServImpl implements IMerchantsServ {
     /**
      * Merchants database interface
      */
-    private MerchantsDao merchantsDao;
+    private final MerchantsDao merchantsDao;
 
-    public MerchantsServImpl(MerchantsDao merchantsDao){
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public MerchantsServImpl(MerchantsDao merchantsDao, KafkaTemplate<String, String> kafkaTemplate){
         this.merchantsDao = merchantsDao;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
+    /**
+     * Create merchants according to requests
+     * @param request {@link CreateMerchantsRequest} the request to create a merchant
+     * @return
+     */
     @Override
     @Transactional
     public Response createMerchants(CreateMerchantsRequest request) {
@@ -48,13 +63,46 @@ public class MerchantsServImpl implements IMerchantsServ {
     }
 
 
+    /**
+     * build merchant by its ID
+     * @param id the id of the merchant to be built
+     * @return {@link Response} Response
+     */
     @Override
     public Response buildMerchantsInfoById(Integer id) {
-        return null;
+        Response response = new Response();
+        Optional<Merchants> merchants = merchantsDao.findById(id);
+        if (merchants == null){
+            response.setErrorCode(ErrorCode.MERCHANTS_NOT_EXIST.getCode());
+            response.setErrorMessage(ErrorCode.MERCHANTS_NOT_EXIST.getDesc());
+        }
+        response.setData(merchants);
+        return response;
     }
 
+    /**
+     * Drop template and get response
+     * @param template {@link PassTemplate} the coupon object
+     * @return {@link Response} Response
+     */
     @Override
     public Response dropPassTemplate(PassTemplate template) {
-        return null;
+        Response response = new Response();
+        ErrorCode errorCode = template.validate(merchantsDao);
+
+        if(errorCode != ErrorCode.SUCCESS){
+            response.setErrorCode(errorCode.getCode());
+            response.setErrorMessage(errorCode.getDesc());
+        }
+        else{
+            String passTemplate = JSON.toJSONString(template);
+            kafkaTemplate.send(
+                    Constants.TEMPLATE_TOPIC,//topic
+                    Constants.TEMPLATE_TOPIC,//key
+                    passTemplate//value
+            );
+            log.info("DropPassTemplate: {}", passTemplate);
+        }
+        return response;
     }
 }
--- src/test/java/com/cardcoupon/passbook/service/MerchantsServTest.java
+++ src/test/java/com/cardcoupon/passbook/service/MerchantsServTest.java
@@ -3,11 +3,15 @@ package com.cardcoupon.passbook.service;
 import com.alibaba.fastjson.JSON;
 import com.cardcoupon.passbook.service.IMerchantsServ;
 import com.cardcoupon.passbook.vo.CreateMerchantsRequest;
+import com.cardcoupon.passbook.vo.PassTemplate;
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 /**
  * Test Class for Merchants Service
@@ -28,6 +32,33 @@ public class MerchantsServTest {
         request.setAddress("Pittsburgh,PA");
 
         System.out.println(JSON.toJSONString(merchantsServ.createMerchants(request)));
+    }
+
+    @Test
+    public void  testBuildMerchantsInfoById(){
+        System.out.println(
+                JSON.toJSONString(merchantsServ.buildMerchantsInfoById(9))
+        );
+    }
+
+    @Test
+    public void testDropPass(){
+        PassTemplate passTemplate = new PassTemplate();
+        passTemplate.setId(9);
+        passTemplate.setTitle("Icecream 2021");
+        passTemplate.setSummary("Summary: Icecream coupoun for 2021");
+        passTemplate.setDesc("Description:Can be uese in Pennsylvania.");
+        passTemplate.setLimit(100000L);
+        passTemplate.setHasToken(false);
+        passTemplate.setBackground(2);
+        passTemplate.setStart(DateUtils.addDays(new Date(),-20));
+        passTemplate.setEnd(DateUtils.addDays(new Date(),20));
+
+        System.out.println(
+                JSON.toJSONString(merchantsServ.dropPassTemplate(passTemplate))
+        );
+
+
     }
 
 
