a1c150424c6c098071ebfabbbe66397d267f87e5
Di Lu <dilu@andrew.cmu.edu>
2021-06-29 02:06:28 -0400

    add README.md details

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# CardCoupon
+# CardCoupon Merchants side Application
+This is the subsystem for Merchant side.
+You can run the application
+
+# Requirements
+For building and running the application you need:
+
+<a href="https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html">JDK 1.8</a>
+<a href="https://maven.apache.org/">Maven</a>
+
+# Running the project
+There are several ways to run a Spring Boot application on your local machine. 
+One way is to execute the main method in 
+*the src/main/java/com/cardcoupon/passbook/MerchantsApplication.java* 
+class from your IDE.
+
+```
+mvn clean install
+```
 
 ## start sql:
 ```
@@ -25,3 +43,55 @@ bin/kafka-topics.sh --describe --topic merchants-template --bootstrap-server loc
 ```
 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic merchants-template --from-beginning
 ```
+
+## url request
+1. get merchants from database
+```
+http://localhost:9527/merchants/{id}
+```
+#### example:
+![img_2.png](img_2.png)
+
+
+2. create merchants
+### get merchants from database
+```
+http://localhost:9527/merchants/create
+```
+#### example:
+```
+{
+    "name": "Amazon",
+    "logoUrl": "www.amazon.com",
+    "businessLicenseUrl": "www.amazon.com",
+    "phone": "909-000-1111",
+    "address": "San Diego,CA"
+}
+```
+![img_3.png](img_3.png)
+
+there should be new records in mysql database.
+![img_4.png](img_4.png)
+
+3. drop template
+### get merchants from database
+```
+http://localhost:9527/merchants/create
+```
+#### example:
+```
+{
+    "background":3,
+    "desc":"this is a coupoun for vitamins",
+    "end":1626665870000,
+    "hasToken":false,
+    "id":11,
+    "limit":10000,
+    "start":1623209872000,
+    "summary":"Summary: vitamin coupoun for CVS",
+    "title":"CVS Vitamin 2021"
+}
+```
+there shoule be new templates in KAFKA
+![img_6.png](img_6.png)
+![img_5.png](img_5.png)
--- src/test/java/com/cardcoupon/passbook/service/MerchantsServTest.java
+++ src/test/java/com/cardcoupon/passbook/service/MerchantsServTest.java
@@ -22,37 +22,63 @@ public class MerchantsServTest {
     @Autowired
     private IMerchantsServ merchantsServ;
 
+    /**
+     * {
+     *  "data":{"id":19},
+     *  "errorCode":0,
+     *  "errorMessage":""
+     * }
+     */
     @Test
     public void testCreateMerchantServ(){
         CreateMerchantsRequest request = new CreateMerchantsRequest();
-        request.setName("Google");
-        request.setLogoUrl("www.google.com");
-        request.setBusinessLicenseUrl("www.google.com");
-        request.setPhone("412-000-2021");
-        request.setAddress("Pittsburgh,PA");
+        request.setName("Costco");
+        request.setLogoUrl("www.costco.com");
+        request.setBusinessLicenseUrl("www.costco.com");
+        request.setPhone("151-111-3456");
+        request.setAddress("Philadelphia,PA");
 
         System.out.println(JSON.toJSONString(merchantsServ.createMerchants(request)));
     }
 
+    /***
+     * {"data":
+     *   {
+     *      "address":"Pittsburgh,PA",
+     *      "businessLicenseUrl":"www.google.com",
+     *      "id":17,"isAudit":false,
+     *      "logoUrl":"www.google.com",
+     *      "name":"Google",
+     *      "phone":"412-000-2021"
+     *    },
+     *  "errorCode":0,
+     *  "errorMessage":""}
+     */
     @Test
     public void  testBuildMerchantsInfoById(){
         System.out.println(
-                JSON.toJSONString(merchantsServ.buildMerchantsInfoById(9))
+                JSON.toJSONString(merchantsServ.buildMerchantsInfoById(17))
         );
     }
 
+    /**
+     * {
+     *  "errorCode":0,
+     *  "errorMessage":""
+     *  }
+     */
     @Test
     public void testDropPass(){
         PassTemplate passTemplate = new PassTemplate();
-        passTemplate.setId(9);
-        passTemplate.setTitle("Icecream 2021");
-        passTemplate.setSummary("Summary: Icecream coupoun for 2021");
-        passTemplate.setDesc("Description:Can be uese in Pennsylvania.");
-        passTemplate.setLimit(100000L);
+        passTemplate.setId(11);
+        passTemplate.setTitle("Sunscreen in Giant Eagle");
+        passTemplate.setSummary("Summary: Sunscreen coupon for 2021");
+        passTemplate.setDesc("Description:Can be used in all Giant Eagle store");
+        passTemplate.setLimit(150000L);
         passTemplate.setHasToken(false);
-        passTemplate.setBackground(2);
-        passTemplate.setStart(DateUtils.addDays(new Date(),-20));
-        passTemplate.setEnd(DateUtils.addDays(new Date(),20));
+        passTemplate.setBackground(4);
+        passTemplate.setStart(DateUtils.addDays(new Date(),-55));
+        passTemplate.setEnd(DateUtils.addDays(new Date(),55));
 
         System.out.println(
                 JSON.toJSONString(merchantsServ.dropPassTemplate(passTemplate))
